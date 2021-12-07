42766b339f527d7d4973f223646e77c98d8b7e87
Eugene Molari <eugelogic@gmail.com>
2021-12-07 14:40:12 +0000

    Comment out TypeScript from Scroll Triggered example

--- pages/scrollTriggered.js
+++ pages/scrollTriggered.js
@@ -1,13 +1,14 @@
 import { motion, Variants } from "framer-motion"
 import styles from "../styles/ScrolledTriggered.module.css"
 
-interface Props {
-  emoji: string
-  hueA: number
-  hueB: number
-}
+// interface Props {
+//   emoji: string
+//   hueA: number
+//   hueB: number
+// }
 
-const cardVariants: Variants = {
+// const cardVariants: Variants = {
+const cardVariants = {
   offscreen: {
     y: 300
   },
@@ -22,9 +23,11 @@ const cardVariants: Variants = {
   }
 }
 
-const hue = (h: number) => `hsl(${h}, 100%, 50%)`
+// const hue = (h: number) => `hsl(${h}, 100%, 50%)`
+const hue = (h) => `hsl(${h}, 100%, 50%)`
 
-function Card({ emoji, hueA, hueB }: Props) {
+// function Card({ emoji, hueA, hueB }: Props) {
+function Card({ emoji, hueA, hueB }) {
   const background = `linear-gradient(306deg, ${hue(hueA)}, ${hue(hueB)})`
 
   return (
@@ -42,7 +45,8 @@ function Card({ emoji, hueA, hueB }: Props) {
   )
 }
 
-const food: [string, number, number][] = [
+// const food: [string, number, number][] = [
+const food = [
   ["🍅", 340, 10],
   ["🍊", 20, 40],
   ["🍋", 60, 90],
